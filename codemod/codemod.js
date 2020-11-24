21dc52c2ab81f93df2459d686659e7016ec347d7
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-11-25 00:01:07 +0100

    Add codemod parts not present in the slides

--- codemod/codemod.js
+++ codemod/codemod.js
@@ -1,4 +1,8 @@
-module.exports = function plugin() {
+function upper(str) {
+  return str[0].toUpperCase() + str.slice(1);
+}
+
+module.exports = function plugin({ types: t, template }) {
   // 1. Detecting React class components (slide 93)
   function isReactComponent(node) {
     return t.matchesPattern(node.superClass, "React.Component");
@@ -14,7 +18,18 @@ module.exports = function plugin() {
         }) && t.isIdentifier(el.key, { name })
     );
     if (elem) return elem.body.body;
-  }
+	}
+
+	// 4. Convert state = {…} to useState() (slide 123)
+	function findField(node, name) {
+		const elem = node.body.body.find(el =>
+			t.isClassProperty(el, {
+				static: false, computed: false
+			})
+			&& t.isIdentifier(el.key, { name })
+		);
+		if (elem) return elem.value;
+	}
 
   // 3. Rewriting this.props usage (slide 115)
   function rewritePropsUsage(path) {
@@ -39,11 +54,12 @@ module.exports = function plugin() {
 
     // 🔍 Complex state initialization (slide 147)
     if (!t.isObjectExpression(stateNode)) {
-      t.addComment(
-        stateNode.node,
-        "leading",
-        " @warning: Unable to refactor complex state initialization "
-      );
+      if (!node.comments) node.comments = [];
+      node.comments.push({
+        leading: true,
+        value: " @warning: Unable to refactor complex state initialization ",
+        type: "CommentBlock",
+      });
       return;
     }
 
@@ -58,22 +74,75 @@ module.exports = function plugin() {
     return state;
   }
 
+  // 4. Convert this.state.count to count (slide 135 - code not shown)
+  function rewriteStateRead(path, state) {
+    path.traverse({
+      MemberExpression(path) {
+        if (
+          path.get("object").isMemberExpression({ computed: false }) &&
+          path.get("object.object").isThisExpression() &&
+          path.get("object.property").isIdentifier({ name: "state" })
+        ) {
+          path.replaceWith(t.cloneNode(state.get(path.node.property.name).get));
+        }
+      },
+    });
+  }
+
+  // 4. Convert this.setState to setCount (slide 136 - code not shown)
+  function rewriteStateUpdate(path, state) {
+    path.traverse({
+      CallExpression(path) {
+        if (
+          path.get("callee").isMemberExpression({ computed: false }) &&
+          path.get("callee.object").isThisExpression() &&
+          path.get("callee.property").isIdentifier({ name: "setState" }) &&
+          path.get("arguments").length === 1
+        ) {
+          const arg = path.get("arguments.0");
+
+          const updateNodes = [];
+          for (const prop of arg.get("properties")) {
+            const { name } = prop.node.key;
+            updateNodes.push(
+              template.ast`
+								${state.get(name).set}(${prop.node.value})
+							`
+            );
+          }
+
+          path.replaceWithMultiple(updateNodes);
+        }
+      },
+    });
+  }
+
   // 5. Convert class fields to variables (slide 172)
   function findClassProperties(node) {
     const properties = new Map();
     for (const elem of node.body.body) {
-      if (
-        !t.isClassProperty(elem, {
-          computed: false,
-          static: false,
-        })
-      ) {
-        continue;
+      if (t.isClassProperty(elem, { computed: false, static: false })) {
+        const { name } = elem.key;
+        if (name === "state") continue;
+
+        properties.set(name, elem.value);
       }
+      // BONUS (not in the slides) - Transform class methods
+      else if (t.isClassMethod(elem, { computed: false, static: false })) {
+        const { name } = elem.key;
+        if (
+          name === "render" ||
+          name === "componentDidMount" ||
+          name === "componentDidUnmount"
+        ) {
+          continue;
+        }
 
-      if (elem.key.name === "state") continue;
-
-      properties.set(elem.key.name, elem.value);
+        properties.set(
+          name,
+          t.arrowFunctionExpression(elem.params, elem.body)
+        );
+      }
     }
     return properties;
   }
@@ -94,18 +163,54 @@ module.exports = function plugin() {
     });
   }
 
-  // 6. Inject imports for used hooks (slide 204)
+  // 6. Inject imports for used hooks (slide 203)
   function findReactImport(path) {
     const program = path.findParent((p) => p.isProgram());
     let importPath = program
       .get("body")
-      .find(({ node }) => t.isImportDeclaration(node))
+      .filter(({ node }) => t.isImportDeclaration(node))
       .find(({ node }) => node.source.value === "react");
 
     if (importPath) return importPath;
 
     program.unshiftContainer("body", template.ast`import "react"`);
     return program.get("body.0");
+  }
+
+  // BONUS (not in the slides) - Output a "warning" comment when remains a 'this'
+  function warnRemaingingThis(path) {
+    path.traverse({
+      ThisExpression({ node }) {
+        if (!node.comments) node.comments = [];
+        node.comments.push({
+          leading: true,
+          value: " @warning: Unhandled 'this' ",
+          type: "CommentBlock",
+        });
+      },
+    });
+  }
+
+  // BONUS (not in the slides) - Support componentDidMount and componentDidUnmount
+  function extractLifecycleHooks(node) {
+    const effects = [];
+    const componentDidMount = findMethod(node, "componentDidMount");
+    if (componentDidMount) {
+      effects.push(
+        template.ast`
+					useEffect(() => { ${componentDidMount} }, []);
+        `
+      );
+    }
+    const componentDidUnmount = findMethod(node, "componentDidUnmount");
+    if (componentDidUnmount) {
+      effects.push(
+        template.ast`
+					useEffect(() => () => { ${componentDidUnmount} }, []);
+        `
+      );
+    }
+    return effects;
   }
 
   return {
@@ -123,10 +228,16 @@ module.exports = function plugin() {
         const state = findInitialState(path.node);
         const useStateCalls = [];
         if (state) {
-          for (let { get, set, init } of state.values())
+          for (let { get, set, init } of state.values()) {
             useStateCalls.push(template.ast`
 							const [${get}, ${set}] = useState(${init})
 						`);
+          }
+
+          // 4. Convert this.state.count to count (slide 135 - code not shown)
+          rewriteStateRead(path, state);
+          // 4. Convert this.setState to setCount (slide 136 - code not shown)
+          rewriteStateUpdate(path, state);
         }
 
         // 5. Convert class fields to variables (slide 179)
@@ -140,22 +251,40 @@ module.exports = function plugin() {
         // 5. Convert class fields to variables (slide 188)
         rewriteVarsUsage(path, vars);
 
+        // BONUS (not in the slides) - Support componentDidMount and componentDidUnmount
+        const useEffectCalls = extractLifecycleHooks(path.node);
+
+        // BONUS (not in the slides) - Output a "warning" comment when remains a 'this'
+        warnRemaingingThis(path);
+
         // 2. Extracting the render() method (slide 104)
         path.replaceWith(template.ast`
 					const ${path.node.id} = (props) => {
 						${/* (slide 132) */ useStateCalls};
 						${/* (slide 179) */ varsDeclarations};
+						${/* BONUS (not in the slides) */ useEffectCalls};
 						${findMethod(path.node, "render")};
 					};
 				`);
 
-				// 6. Inject imports for used hooks (slide 218)
+        // 6. Inject imports for used hooks (slide 217)
         if (useStateCalls.length > 0 && !path.scope.hasBinding("useState")) {
           findReactImport(path).pushContainer(
             "specifiers",
             t.importSpecifier(
               t.identifier("useState"),
               t.identifier("useState")
+            )
+          );
+        }
+
+        // BONUS (not in the slides) - Support componentDidMount and componentDidUnmount
+        if (useEffectCalls.length > 0 && !path.scope.hasBinding("useEffect")) {
+          findReactImport(path).pushContainer(
+            "specifiers",
+            t.importSpecifier(
+              t.identifier("useEffect"),
+              t.identifier("useEffect")
             )
           );
         }
